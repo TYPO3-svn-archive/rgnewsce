857bdee44bec3fc0f52af583a6dac4562768c413
t33k <t33k@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-30 10:42:32 +0000

    update of ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rgnewsce/trunk@11539 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "rgnewsce"
 #
-# Auto generated 13-03-2008 19:53
+# Auto generated 30-08-2008 12:40
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Content elements in tt_news',
 	'description' => 'Add normal content elements to tt_news',
 	'category' => 'fe',
-	'author' => 'Georg Ringer',
+	'author' => 'Georg Ringer / Krystian Szymukowicz',
 	'author_email' => 'http://www.ringer.it',
 	'shy' => '',
 	'dependencies' => 'tt_news',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'tt_news' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"db93";s:10:"README.txt";s:4:"ee2d";s:24:"class.tx_rgnewsce_fe.php";s:4:"f053";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"171c";s:14:"ext_tables.php";s:4:"51d9";s:14:"ext_tables.sql";s:4:"aafd";s:16:"locallang_db.xml";s:4:"f35c";s:19:"doc/wizard_form.dat";s:4:"3203";s:20:"doc/wizard_form.html";s:4:"28da";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"3d2d";s:10:"README.txt";s:4:"37eb";s:24:"class.tx_rgnewsce_fe.php";s:4:"6588";s:21:"ext_conf_template.txt";s:4:"ff0f";s:12:"ext_icon.gif";s:4:"5e2a";s:17:"ext_localconf.php";s:4:"04bb";s:14:"ext_tables.php";s:4:"0443";s:14:"ext_tables.sql";s:4:"5eae";s:16:"locallang_db.xml";s:4:"3e98";s:14:"doc/manual.sxw";s:4:"ec82";s:19:"doc/wizard_form.dat";s:4:"3203";s:20:"doc/wizard_form.html";s:4:"594a";s:16:"res/rgnewsce.css";s:4:"8eec";}',
 	'suggests' => array(
 	),
 );
